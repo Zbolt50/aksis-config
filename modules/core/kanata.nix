8dd0e83b19e33fab0f191b0d677e75f0ca3ce0d8
Zbolt50 <zekeboggs10@gmail.com>
2025-03-31 21:53:11 -0400

    fixed kanata issues with tty sessions

--- modules/core/kanata.nix
+++ modules/core/kanata.nix
@@ -1,7 +1,19 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
     # Enable kanata for home-row-mods. 
     # Gonna make this a toggleable module later. 
+
+    boot.kernelModules = [ "uinput" ]; 
+    hardware.uinput.enable = true;
+    services.udev.extraRules = ''
+        KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+    '';
+    systemd.services.kanata-internalKeyboard.serviceConfig = {
+        SupplementaryGroups = [
+            "input"
+            "uinput"
+        ];
+    };
     services.kanata = {
         enable = true;
         keyboards = {
@@ -9,31 +21,36 @@
                 # Specify kanata for specific boards
                 # Good for QMK boards with this enabled
                 devices = [
+                    # Use `ls /dev/input/by-path/` to find your keyboard devices.
                     "/dev/input/by-path/platform-i8042-serio-0-event-kbd" 
+                    "/dev/input/by-path/pci-0000:00:14.0-usb-0:3:1.0-event-kbd"
+                    # Look for another device if this doesnt work
                 ];
-                extraDefCfg = "process-unmapped-keys yes";
+                extraDefCfg = ''
+                    process-unmapped-keys yes
+                '';
                 config = ''
                     (defsrc
                         caps a s d f j k l ;
                     ) 
                     (defvar
-                        tap-time 150
-                        hold-time 200
+                        tap-time 250
+                        hold-time 250
                     )
 
                     (defalias
-                        escctrl (tap hold 100 100 esc lctl)
-                        a (multi f24 (tap-hold $tap-time $hold-time a lsft))
-                        s (multi f24 (tap-hold $tap-time $hold-time s lctl))
-                        d (multi f24 (tap-hold $tap-time $hold-time d lalt))
-                        f (multi f24 (tap-hold $tap-time $hold-time f lmet))
-                        j (multi f24 (tap-hold $tap-time $hold-time j rmet))
-                        k (multi f24 (tap-hold $tap-time $hold-time k ralt))
-                        l (multi f24 (tap-hold $tap-time $hold-time l rctl))
-                        ; (multi f24 (tap-hold $tap-time $hold-time ; rsft))
+                        escctrl (tap-hold 100 100 esc lctl)
+                        a (multi (tap-hold $tap-time $hold-time a lsft))
+                        s (multi (tap-hold $tap-time $hold-time s lctl))
+                        d (multi (tap-hold $tap-time $hold-time d lalt))
+                        f (multi (tap-hold $tap-time $hold-time f lmet))
+                        j (multi (tap-hold $tap-time $hold-time j rmet))
+                        k (multi (tap-hold $tap-time $hold-time k ralt))
+                        l (multi (tap-hold $tap-time $hold-time l rctl))
+                        ; (multi (tap-hold $tap-time $hold-time ; rsft))
                     )
                     (deflayer base
-                        @esctrl @a @s @d @f @j @k @l @;
+                        @escctrl @a @s @d @f @j @k @l @;
                     )
                 '';
             };
