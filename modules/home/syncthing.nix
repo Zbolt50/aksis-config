1cc3157c6b5a9c8354897597a9fa798c77fce591
Zbolt50 <zekeboggs10@gmail.com>
2025-04-20 12:42:39 -0400

    Fixing syncthing issues

--- modules/home/syncthing.nix
+++ modules/home/syncthing.nix
@@ -10,8 +10,37 @@
     overrideDevices = true;
     overrideFolders = true;
     settings = {
-      devices = { };
-      folders = { };
+      devices = {
+        "aksis" = {
+          id = "IVR4HNF-H772KYL-626U5VX-77IVCE4-IQUMVW6-ZZC2OG7-5XTCWLA-SABFJQD";
+        };
+        "saladin" = {
+          id = "JSQEZ7I-JBHJRZU-C7Z5GFS-6WUWFHG-X4HYIW5-6ODZYEH-7FFWDCA-EYVH3QF";
+        };
+      };
+      folders = {
+        "Music" = {
+          path = "~/Music";
+          devices = [
+            "aksis"
+            "saladin"
+          ];
+        };
+        "Projects" = {
+          path = "~/Projects";
+          devices = [
+            "aksis"
+            "saladin"
+          ];
+        };
+        "Vaults" = {
+          path = "~/Vaults";
+          devices = [
+            "aksis"
+            "saladin"
+          ];
+        };
+      };
     };
   };
 }
